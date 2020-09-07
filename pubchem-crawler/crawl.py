dabb797bb73c75bbbc1000ce5e228589538c1b20
Ruslan Khyurri <ruslan_khyurri@epam.com>
2020-09-07 17:08:10 +0300

    Add bingo database support to crawl.py

--- pubchem-crawler/crawl.py
+++ pubchem-crawler/crawl.py
@@ -1,11 +1,15 @@
+import argparse
 import ftplib
+import gzip
 import logging
+import os
 import time
 from pathlib import Path
-from typing import Generator, Iterable, List, Set
-import argparse
+from typing import Callable, Generator, Iterable, List, Set
 
+import indigo
 from ftpretty import ftpretty
+from indigo.bingo import Bingo
 
 
 class FTP:
@@ -60,7 +64,7 @@ class PubchemLoader:
             elif not self.__checksum(pth):
                 self.__download_file(ftp_, filename, tries + 1)
             else:
-                logging.error('File %s already downloaded', filename)
+                logging.error("File %s already downloaded", filename)
 
     @staticmethod
     def get_names(dir_: Iterable) -> Set[str]:
@@ -88,7 +92,6 @@ class PubchemLoader:
         source_dir = f"{self.target}/CURRENT-Full/{self.fmt}"
 
         with FTP(self.ftp, "anonymous", "anonymous@domain.com") as ftp_:
-            # download md5
             for ext_ in self.sup_extensions:
                 for remote_file in self.__diff(
                     source_dir, ftp_.list(source_dir), ext_[1:]
@@ -103,23 +106,109 @@ class PubchemLoader:
                         self.full_download()
 
 
-if __name__ == "__main__":
+class Extractor:
+    def __init__(self, chembl_dir):
+        self.chembl_dir = Path(chembl_dir)
 
-    parser = argparse.ArgumentParser(description='Pubchem crawler')
-    parser.add_argument('mode', type=str,
-                        help='Modes: download')
-    parser.add_argument('--pubchem-dir',
-                        type=str,
-                        default='./pubchem_dir',
-                        help='Pubchem directory path')
+    @staticmethod
+    def __gunzip_content(file: Path) -> Path:
+        target_file = file.with_suffix("")
+        with open(target_file, "wb") as target, gzip.open(file, "rb") as src:
+            target.write(src.read())
+        return target_file
+
+    @staticmethod
+    def cast_path(root: Path, files: List[str]) -> Generator[Path, None, None]:
+        for file in files:
+            yield root / file
+
+    def extract(self, handler: Callable[[Path], None]):
+        for root, dirs, files in os.walk(self.chembl_dir):
+            for file in self.cast_path(Path(root), files):
+                if file.suffix == ".gz":
+                    try:
+                        extracted = self.__gunzip_content(file)
+                        handler(extracted)
+                        extracted.unlink(missing_ok=True)
+                    except EOFError as err_:
+                        logging.error("File %s: %s", file, err_)
+
+
+class BingoNoSQLDatabase:
+    def __init__(self, arg_ns: argparse.Namespace):
+        self.arg_ns = arg_ns
+        self.session = indigo.Indigo()
+
+        if arg_ns.bingo_dir:
+            self.bingo_dir = arg_ns.bingo_dir
+        else:
+            self.bingo_dir = Path(arg_ns.pubchem_dir) / "bingo_dir"
+
+        if self.bingo_dir.is_dir():
+            self.bingo_conn = Bingo.loadDatabaseFile(
+                self.session, str(self.bingo_dir), ""
+            )
+        else:
+            self.bingo_conn = Bingo.createDatabaseFile(
+                self.session, str(self.bingo_dir), "molecule", ""
+            )
+
+    def handler(self, source_file: Path):
+        for molecule in self.session.iterateSDFile(str(source_file)):
+            try:
+                self.bingo_conn.insert(molecule)
+            except indigo.bingo.BingoException as e:
+                logging.error("Cannot upload molecule: %s", e)
+
+
+class ElasticDatabase:
+    def __init__(self, arg_ns: argparse.Namespace):
+        pass
+
+    def handler(self, source_file: Path):
+        pass
+
+
+def download(arg_ns: argparse.Namespace) -> None:
+    loader = PubchemLoader("compounds", arg_ns.pubchem_dir, "sdf")
+    loader.full_download()
+
+
+def extract(arg_ns: argparse.Namespace) -> None:
+    extractor = Extractor(arg_ns.pubchem_dir)
+    if arg_ns.database == "bingo_nosql":
+        extractor.extract(BingoNoSQLDatabase(arg_ns).handler)
+
+
+if __name__ == "__main__":
 
     logging.basicConfig(level=logging.INFO)
 
+    parser = argparse.ArgumentParser(description="Pubchem crawler")
+
+    subparsers = parser.add_subparsers(help="select mode: download, extract")
+
+    parser_download = subparsers.add_parser("download")
+    parser_download.set_defaults(func=download)
+    parser_download.add_argument(
+        "--pubchem-dir",
+        type=str,
+        default="./pubchem_dir",
+        help="Pubchem directory path",
+    )
+
+    parser_extract = subparsers.add_parser("extract")
+    parser_extract.add_argument("--database", type=str, default="bingo_nosql")
+    parser_extract.add_argument(
+        "--pubchem-dir",
+        type=str,
+        default="./pubchem_dir",
+        help="Pubchem directory path",
+    )
+    parser_extract.add_argument(
+        "--bingo-dir", type=str, help="Bingo nosql db location"
+    )
+    parser_extract.set_defaults(func=extract)
+
     args = parser.parse_args()
-    mode = args.mode
-    pubchem_dir = Path(args.pubchem_dir)
-    if mode == 'download':
-        loader = PubchemLoader('compounds', pubchem_dir, 'sdf')
-        loader.full_download()
-    else:
-        logging.error('Unknown mode %s', mode)
+    args.func(args)
