2ccbb9406d0409575431f6fc02d29e0dc30d6d37
Konstantin Perikov <pericov.ko@gmail.com>
2020-12-14 23:15:34 +0000

    create generation of docker-compose with cookiecutter

--- fingerprint-similarity-match/test.py
+++
@@ -1,9 +0,0 @@
-from indigo import *
-
-indigo = Indigo()
-
-m1 = indigo.loadMolecule(
-    "O=C1CC(CN1C1=CC=CC=C1F)C(=O)NCC1=CC(=CC=C1)NC(=O)C1CC1"
-)
-print(m1.canonicalSmiles())
-print(m1.fingerprint(type='sim').oneBitsList())
